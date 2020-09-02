61b5f8c8be23830619008945c32d367cadb8c609
Elena <tchka777@gmail.com>
2020-09-02 08:53:34 +0300

    corr

--- lesson7/Tictaktoe/js/Main.js
+++ lesson7/Tictaktoe/js/Main.js
@@ -1,15 +1,12 @@
 window.addEventListener('load', () => {
     const status = new Status();
     const board = new Board();
-//    const menu = new Menu();
-//    const game = new Game();
 
 
     board.init(status);
-//    game.init(status, board, menu);
 
     board.renderMap();
     board.initEventHandlers();
 
-//    game.run();
+
 });
